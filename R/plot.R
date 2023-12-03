7007028c3227b9ca16f048edde099ed260cd5629
Edvin Tran Hoac <etranhoac@wesleyan.edu>
2023-12-02 21:46:50 -0500

    Legend name

--- R/plot.R
+++ R/plot.R
@@ -10,9 +10,10 @@ plot.ryx <- function(x) {
   df$variable <- factor(df$variable, levels = df$variable[order(df$absr)])
 
   ggplot(df, aes(x = absr, y = variable, absr)) +
-    geom_point(aes(color = sign), size = 2) +
+    geom_point(aes(color = sign), size = 3) +
     geom_segment(aes(x = 0, xend = absr, yend = variable), color = "lightgray") +
-    labs(title = paste0("Correlations with ", x$y), x = "Correlation (absolute value)", y = "Variables") +
+    labs(title = paste0("Correlations with ", x$y), x = "Correlation (absolute value)", y = "Variables",
+         color = "Direction") +
     scale_x_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.1)) +
     scale_color_manual(values = c("red", "blue")) +
     theme_bw() +
